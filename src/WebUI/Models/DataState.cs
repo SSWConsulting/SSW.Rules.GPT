da2ec1fba4581f3f42a909dbc3c99e956ee4716e
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-05-16 16:28:35 +1000

    Add a cancel request button

--- src/WebUI/Models/DataState.cs
+++ src/WebUI/Models/DataState.cs
@@ -6,4 +6,5 @@ public class DataState
 {
     public string? OpenAiApiKey { get; set; }
     public List<ChatMessage> ChatMessages { get; set; } = new();
+    public CancellationTokenSource CancellationTokenSource = new CancellationTokenSource();
 }
