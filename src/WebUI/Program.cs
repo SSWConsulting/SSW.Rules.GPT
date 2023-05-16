19afcec4c4dbfdfdce8598dea5b4e8e4855038dd
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-05-16 11:25:44 +1000

    catch chatContainer being null

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -21,7 +21,18 @@ builder.Services.AddSingleton<SignalRClient>();
 builder.Services.AddSingleton<NotifierService>();
 builder.Services.AddScoped<SswRulesGptDialogService>();
 builder.Services.AddScoped<ApiKeyValidationService>();
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 builder.Services.AddMudMarkdownServices();
 
 const string ApiClient = nameof(ApiClient);
