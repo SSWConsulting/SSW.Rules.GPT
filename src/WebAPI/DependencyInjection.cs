926ba7c87b747a7f54fbf2cc5c87b526c9ed4771
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-05-19 11:11:52 +1000

    re-enable app insights

--- src/WebAPI/DependencyInjection.cs
+++ src/WebAPI/DependencyInjection.cs
@@ -26,7 +26,7 @@ public static class DependencyInjection
         }
         else
         {
-            //services.AddApplicationInsightsTelemetry();
+            services.AddApplicationInsightsTelemetry();
             services.AddLogging(
                 options =>
                     options
