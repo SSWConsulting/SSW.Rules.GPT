d7f4d1682e8a3ae4af32b0b5145839788f3af787
Jack Reimers <jack.reimers.00@gmail.com>
2023-06-09 12:01:49 +1000

    Revert "Move 'New Chat' button from top right dropdown menu to hover above message input"
    This reverts commit 64c34ff2410e8d70250ecf9f8c7543573b9f2122.

--- src/WebUI/Models/DataState.cs
+++ src/WebUI/Models/DataState.cs
@@ -1,28 +1,13 @@
 ﻿using OpenAI.GPT3.ObjectModels.RequestModels;
-using WebUI.Services;
 
 namespace WebUI.Models;
 
 public class DataState
 {
-    private readonly NotifierService _notifierService;
-    
-    public DataState(NotifierService notifierService)
-    {
-        _notifierService = notifierService;
-    }
-    
     public string? OpenAiApiKey { get; set; }
     public List<ChatMessage> ChatMessages { get; set; } = new();
     public CancellationTokenSource CancellationTokenSource = new CancellationTokenSource();
     public OpenAI.GPT3.ObjectModels.Models.Model SelectedGptModel { get; set; } =
         OpenAI.GPT3.ObjectModels.Models.Model.ChatGpt3_5Turbo;
     public bool UsingByoApiKey { get; set; } = false;
-
-    public async Task ClearHistory()
-    {
-        await _notifierService.CancelMessageStream();
-        ChatMessages.Clear();
-        await _notifierService.Update();
-    }
 }
