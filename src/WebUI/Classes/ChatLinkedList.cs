6388d0bb7c851a84c37b9e5a2faa126575d3e6f7
Jack Reimers [SSW] <10693364+jackreimers@users.noreply.github.com>
2023-06-12 09:39:59 +1000

    ✨New Feature - Edit and Resend a Question (#77)
    * Move chat bubble into its own component
    
    * Add edit message button
    
    * Move RulesBotChat code block to separate file
    
    * Implement linked list for handling multiple message threads
    
    * Only show edit button when hovering the message
    
    * Implement UI controls for editing messages and switching between threads
    
    * Style multiline input field and have it autoscale to fit content
    
    * Fix issue with newlines not displaying after submit

---
+++ src/WebUI/Classes/ChatLinkedList.cs
@@ -0,0 +1,95 @@
+﻿using OpenAI.GPT3.ObjectModels.RequestModels;
+
+namespace WebUI.Classes;
+
+public class ChatLinkedList : List<ChatLinkedListItem>
+{
+    public ChatLinkedListItem Add(ChatMessage message)
+    {
+        var newItem = new ChatLinkedListItem(message);
+        Add(newItem);
+        
+        return newItem;
+    }
+    
+    public ChatLinkedListItem AddAfter(ChatMessage message, ChatLinkedListItem item)
+    {
+        if (item != null && !Contains(item))
+            throw new ArgumentException("Item is not in the list");
+
+        var newItem = new ChatLinkedListItem(message, item);
+        item.Next = newItem;
+        Add(newItem);
+        
+        return newItem;        
+    }
+    
+    public ChatLinkedListItem AddRight(ChatMessage newMessage, ChatLinkedListItem item)
+    {
+        if (!Contains(item))
+            throw new ArgumentException("Item is not in the list");
+
+        var target = item;
+        
+        while (target.Right is not null)
+        {
+            target = target.Right;
+        }
+        
+        var newItem = new ChatLinkedListItem(newMessage, item.Previous, target);
+
+        newItem.Left = target;
+        target.Right = newItem;
+
+        Add(newItem);
+        Move(item, Direction.Right);
+
+        return newItem;
+    }
+    
+    public void Move(ChatLinkedListItem item, Direction direction)
+    {
+        if (!Contains(item))
+            throw new ArgumentException("Item is not in the list");
+        
+        var target = direction == Direction.Left 
+            ? item.Left
+            : item.Right;
+
+        if (target == null)
+        {
+            return;
+        }
+
+        if (item.Previous != null)
+        {
+            item.Previous.Next = target;
+        }
+    }
+
+    public List<ChatLinkedListItem> GetThread(ChatLinkedListItem item)
+    {
+        var head = GetHead(item);
+        var thread = new List<ChatLinkedListItem>();
+        var target = head;
+
+        //Move from top of list to bottom
+        while (target is not null)
+        {
+            thread.Add(target);
+            target = target.Next;
+        }
+
+        return thread;
+    }
+
+    private ChatLinkedListItem GetHead(ChatLinkedListItem item)
+    {
+        var head = item;
+        
+        while (head.Previous is not null)
+            head = head.Previous;
+
+        return head;
+    }
+}
