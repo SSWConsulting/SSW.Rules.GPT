3450ac5ae145a7daf409b9f03fc687f907453415
Jack Reimers <jack.reimers.00@gmail.com>
2023-08-24 11:45:38 +1000

    Clean up ApiKeyValidationService

--- src/WebUI/Services/ApiKeyValidationService.cs
+++ src/WebUI/Services/ApiKeyValidationService.cs
@@ -8,27 +8,20 @@ namespace WebUI.Services;
 
 public class ApiKeyValidationService
 {
-    //private readonly OpenAIService _openAiService;
-
     public async Task<bool> ValidateApiKey(string apiKey, AvailableGptModels gptModel)
     {
         var model = (OpenAI.GPT3.ObjectModels.Models.Model)gptModel;
-        var customAiService = new OpenAIService(new OpenAiOptions() { ApiKey = apiKey });
+        var customAiService = new OpenAIService(new OpenAiOptions { ApiKey = apiKey });
         var completionResult = await customAiService.ChatCompletion.CreateCompletion(
-            new ChatCompletionCreateRequest()
+            new ChatCompletionCreateRequest
             {
-                Messages = new List<ChatMessage> { new ChatMessage("user", "a") },
+                Messages = new List<ChatMessage> { new("user", "a") },
                 MaxTokens = 1,
                 Temperature = (float)0.5
             },
             model.EnumToString()
         );
 
-        if (completionResult.Successful is false)
-        {
-            return false;
-        }
-
-        return true;
+        return completionResult.Successful;
     }
 }
